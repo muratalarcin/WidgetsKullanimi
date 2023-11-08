e2e041d646e1a23816fbbb529a8f1a2bcb80670c
Murat Alarcin <murattrbl@icloud.com>
2023-11-08 21:53:53 +0300

    lesson 3

--- app/src/main/java/com/muratalarcin/widgetskullanimi/MainActivity.java
+++ app/src/main/java/com/muratalarcin/widgetskullanimi/MainActivity.java
@@ -2,15 +2,21 @@ package com.muratalarcin.widgetskullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SeekBar;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 import com.muratalarcin.widgetskullanimi.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -71,6 +77,46 @@ public class MainActivity extends AppCompatActivity {
 
         binding.textViewSlider.setText("Slider : " + binding.slider.getProgress());
 
+        binding.slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                binding.textViewSlider.setText("Slider : " +i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        binding.buttonSaat.setOnClickListener(view -> {
+            MaterialTimePicker tp = new MaterialTimePicker.Builder()
+                    .setTitleText("Saat Seç")
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .build();
+
+            tp.show(getSupportFragmentManager(), "");
+
+            tp.addOnPositiveButtonClickListener(view1 -> {
+                binding.editTextSaat.setText(tp.getHour() + " : " + tp.getMinute());
+            });
+        });
+
+        binding.buttonTarih.setOnClickListener(view -> {
+            MaterialDatePicker dp = MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Tarih Seç")
+                    .build();
+
+            dp.show(getSupportFragmentManager(), "");
+
+            dp.addOnPositiveButtonClickListener(view1 -> {
+                SimpleDateFormat df = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss", Locale.getDefault());
+                String tarih = df.format(view1);
+                binding.editTextTarih.setText(tarih);
+            });
+        });
+
         binding.buttonGoster.setOnClickListener(view -> {
             Log.e("Sonuç", "Switch Durum : " + binding.switch1.isChecked());
             if(kontrol) {
@@ -80,6 +126,7 @@ public class MainActivity extends AppCompatActivity {
             }
             String ulke = binding.autoTextView.getText().toString();
             Log.e("Sonuç", "Ülke : " + ulke);
+            Log.e("Sonuç", "Slide : " + binding.slider.getProgress());
         });
 
     }
